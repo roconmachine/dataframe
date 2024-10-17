67a993db9b72847f1b7e3f353267b5fe8c55093a
Rezaul Hasan <roconmachine@gmail.com>
2024-10-18 00:17:02 +0200

    Add files via upload

---
+++ FineGrainedAccess-Dataframe/src/main/java/com/roconmachine/io/dataframe/access/DataframeApplication.java
@@ -0,0 +1,13 @@
+package com.roconmachine.io.dataframe.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataframeApplication.class, args);
+	}
+
+}
